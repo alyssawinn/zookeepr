a2f8de632897b27e97b424d3b050f2f5bf07765e
Alyssa Winn <alyssawinn2@hotmail.com>
2021-03-31 07:56:41 -0600

    add front-end design and api functionality

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true}));
 //parse incoming JSON data
 app.use(express.json());
+//grab all additional front-end files
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -97,6 +99,18 @@ app.post('/api/animals', (req, res) => {
         res.json(req.body);
     } 
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
